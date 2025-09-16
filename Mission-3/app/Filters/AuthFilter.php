aa17937657a7cc39af3da07cc20513ce6fe61ffa
Umeem26 <hisyamk.u5062@gmail.com>
2025-09-16 15:42:44 +0700

    Fix bug CI (writetable session)

---
+++ Mission-3/app/Filters/AuthFilter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthFilter implements FilterInterface
+{
+    /**
+     * This method is called before a controller is executed.
+     *
+     * @param RequestInterface $request
+     * @param array|null       $arguments
+     *
+     * @return mixed
+     */
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // Jika session 'isLoggedIn' tidak ada atau tidak bernilai TRUE
+        if (!session()->get('isLoggedIn')) {
+            // Maka paksa pengguna untuk kembali ke halaman login
+            return redirect()->to(base_url('login'));
+        }
+    }
+
+    /**
+     * This method is called after a controller is executed.
+     *
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     * @param array|null        $arguments
+     *
+     * @return mixed
+     */
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Tidak perlu melakukan apa-apa setelah halaman ditampilkan
+    }
+}
